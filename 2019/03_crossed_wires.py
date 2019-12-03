2fbd252c8b42d45eaaa1f9a0e5b2e628b5c26fde
Anton Levholm <anton.badminton@gmail.com>
2019-12-03 13:34:41 +0100

    Solutions day 3

---
+++ 2019/03_crossed_wires.py
@@ -0,0 +1,170 @@
+"""
+--- Day 3: Crossed Wires ---
+
+The gravity assist was successful, and you're well on your way to the Venus
+refuelling station. During the rush back on Earth, the fuel management system
+wasn't completely installed, so that's next on the priority list.
+
+Opening the front panel reveals a jumble of wires. Specifically, two wires are
+connected to a central port and extend outward on a grid. You trace the path
+each wire takes as it leaves the central port, one wire per line of text (your
+puzzle input).
+
+The wires twist and turn, but the two wires occasionally cross paths. To fix
+the circuit, you need to find the intersection point closest to the central
+port. Because the wires are on a grid, use the Manhattan distance for this
+measurement. While the wires do technically cross right at the central port
+where they both start, this point does not count, nor does a wire count as
+crossing with itself.
+
+For example, if the first wire's path is R8,U5,L5,D3, then starting from the
+central port (o), it goes right 8, up 5, left 5, and finally down 3:
+
+...........
+...........
+...........
+....+----+.
+....|....|.
+....|....|.
+....|....|.
+.........|.
+.o-------+.
+...........
+Then, if the second wire's path is U7,R6,D4,L4, it goes up 7, right 6, down 4,
+and left 4:
+
+...........
+.+-----+...
+.|.....|...
+.|..+--X-+.
+.|..|..|.|.
+.|.-X--+.|.
+.|..|....|.
+.|.......|.
+.o-------+.
+...........
+These wires cross at two locations (marked X), but the lower-left one is
+closer to the central port: its distance is 3 + 3 = 6.
+
+Here are a few more examples:
+
+R75,D30,R83,U83,L12,D49,R71,U7,L72
+U62,R66,U55,R34,D71,R55,D58,R83 = distance 159
+R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
+U98,R91,D20,R16,D67,R40,U7,R15,U6,R7 = distance 135
+What is the Manhattan distance from the central port to the closest
+intersection?
+
+
+--- Part Two ---
+
+It turns out that this circuit is very timing-sensitive; you actually need to
+minimize the signal delay.
+
+To do this, calculate the number of steps each wire takes to reach each
+intersection; choose the intersection where the sum of both wires' steps is
+lowest. If a wire visits a position on the grid multiple times, use the steps
+value from the first time it visits that position when calculating the total
+value of a specific intersection.
+
+The number of steps a wire takes is the total number of grid squares the wire
+has entered to get to that location, including the intersection being
+considered. Again consider the example from above:
+
+...........
+.+-----+...
+.|.....|...
+.|..+--X-+.
+.|..|..|.|.
+.|.-X--+.|.
+.|..|....|.
+.|.......|.
+.o-------+.
+...........
+In the above example, the intersection closest to the central port is reached
+after 8+5+5+2 = 20 steps by the first wire and 7+6+4+3 = 20 steps by the
+second wire for a total of 20+20 = 40 steps.
+
+However, the top-right intersection is better: the first wire takes only
+8+5+2 = 15 and the second wire takes only 7+6+2 = 15, a total of 15+15 = 30
+steps.
+
+Here are the best steps for the extra examples from above:
+
+R75,D30,R83,U83,L12,D49,R71,U7,L72
+U62,R66,U55,R34,D71,R55,D58,R83 = 610 steps
+R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
+U98,R91,D20,R16,D67,R40,U7,R15,U6,R7 = 410 steps
+What is the fewest combined steps the wires must take to reach an intersection?
+"""
+
+
+# Format data
+def format(data):
+    return [{"direction": move[0], "steps": int(move[1:])} for move in data.split(",")]
+
+
+with open("data/03.txt") as f:
+    first_wire = format(f.readline())
+    second_wire = format(f.readline())
+
+"""
+--- Part one ---
+"""
+
+
+def get_movement_coords(d):
+    if d["direction"] == "U":
+        return 0, d["steps"]
+    if d["direction"] == "D":
+        return 0, -d["steps"]
+    if d["direction"] == "L":
+        return -d["steps"], 0
+    if d["direction"] == "R":
+        return d["steps"], 0
+
+
+def find_coordinates(directions):
+    position = [0, 0]
+    coordinates = []
+    for direction in directions:
+        steps_x, steps_y = get_movement_coords(direction)
+        if steps_x > 0:
+            for i in range(1, steps_x + 1):
+                coordinates.append(f"{position[0] + i},{position[1]}")
+        if steps_x < 0:
+            for i in range(1, abs(steps_x) + 1):
+                coordinates.append(f"{position[0] - i},{position[1]}")
+        if steps_y > 0:
+            for i in range(1, steps_y + 1):
+                coordinates.append(f"{position[0]},{position[1] + i}")
+        if steps_y < 0:
+            for i in range(1, abs(steps_y) + 1):
+                coordinates.append(f"{position[0]},{position[1] - i}")
+        position[0] = position[0] + steps_x
+        position[1] = position[1] + steps_y
+    return coordinates
+
+
+def calc_manhattan_distance(x, y):
+    return abs(int(x)) + abs(int(y))
+
+
+first_coords = find_coordinates(first_wire)
+second_coords = find_coordinates(second_wire)
+intersections = set(first_coords) & set(second_coords)
+coords = [i.split(",") for i in intersections]
+print(min([calc_manhattan_distance(c[0], c[1]) for c in coords]))
+
+"""
+--- Part two ---
+"""
+
+
+def calc_steps(coord):
+    first_steps = first_coords.index(coord) + 1
+    second_steps = second_coords.index(coord) + 1
+    return first_steps + second_steps
+
+
+print(min(calc_steps(coord) for coord in intersections))
