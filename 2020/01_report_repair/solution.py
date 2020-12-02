9347a3f3da196bece7cd4fcdf09e268e77f57c66
Anton Levholm <anton.badminton@gmail.com>
2020-12-02 07:52:16 +0100

    Add solution for day 1 2020

---
+++ 2020/01_report_repair/solution.py
@@ -0,0 +1,32 @@
+# Format data
+with open("data.txt") as f:
+    my_numbers = [int(i) for i in f.readlines()]
+
+"""
+--- Part One ---
+"""
+found_it = False
+for i, n in enumerate(my_numbers):
+    if found_it:
+        break
+    for m in my_numbers[i + 1 :]:
+        if n + m == 2020:
+            print(n * m)
+            found_it = True
+            break
+
+"""
+--- Part Two ---
+"""
+found_it = False
+for i, n in enumerate(my_numbers):
+    if found_it:
+        break
+    for j, m in enumerate(my_numbers[i + 1 :]):
+        if found_it:
+            break
+        for k in my_numbers[j + 1 :]:
+            if n + m + k == 2020:
+                print(n * m * k)
+                found_it = True
+                break
