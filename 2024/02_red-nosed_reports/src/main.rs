37aff582e22d7284a06962f8cc5a3e94a1d137ab
Anton Levholm <anton.badminton@gmail.com>
2024-12-04 08:44:50 +0100

    Solution for Day 2 Part 2 (2024)

--- 2024/02_red-nosed_reports/src/main.rs
+++ 2024/02_red-nosed_reports/src/main.rs
@@ -23,22 +23,24 @@ fn is_safe(diff: &[i32]) -> bool {
     return is_same_direction(diff) && is_in_range(diff);
 }
 
+fn calculate_differences(levels: &Vec<i32>) -> Vec<i32> {
+    return levels.windows(2).map(|pair| pair[1] - pair[0]).collect();
+}
+
 fn main() {
     let contents = read_file();
     let mut safe_count = 0;
     let mut dampened_safe_count = 0;
     for report in contents.lines() {
         let levels: Vec<i32> = report.split_whitespace().map(parse_number).collect();
-        let diff: Vec<i32> = levels.windows(2).map(|pair| pair[1] - pair[0]).collect();
+        let diff: Vec<i32> = calculate_differences(&levels);
         if is_safe(&diff) {
             safe_count += 1;
         } else {
-            for index in 0..diff.len() {
-                let mut dampened_diff = diff.clone();
-                if index + 1 < diff.len() && index != 0 {
-                    dampened_diff[index + 1] += dampened_diff[index];
-                }
-                dampened_diff.remove(index);
+            for index in 0..levels.len() {
+                let mut dampened_levels = levels.clone();
+                dampened_levels.remove(index);
+                let dampened_diff = calculate_differences(&dampened_levels);
                 if is_safe(&dampened_diff) {
                     dampened_safe_count += 1;
                     break;
@@ -54,4 +56,5 @@ fn main() {
 
     // Part 2
     println!("Dampened safe count: {}", safe_count + dampened_safe_count);
+    assert_eq!(safe_count + dampened_safe_count, 290);
 }
