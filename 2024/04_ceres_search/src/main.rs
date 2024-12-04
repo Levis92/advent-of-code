109869dcae65cf2d21aa2564a57110c2ced24208
Anton Levholm <anton.badminton@gmail.com>
2024-12-04 20:37:14 +0100

    Refactor Day 4 Part 1 solution (2024)

--- 2024/04_ceres_search/src/main.rs
+++ 2024/04_ceres_search/src/main.rs
@@ -20,32 +20,7 @@ fn format_data() -> Vec<Vec<String>> {
     return letters;
 }
 
-fn get_horizontal_word<'a>(
-    word_range: &Range<usize>,
-    row: &'a Vec<String>,
-    col_index: usize,
-    col_mod: isize
-) -> Vec<&'a String> {
-    return word_range
-        .clone()
-        .filter_map(|i| Some(&row[(col_index as isize + (col_mod * i as isize)) as usize]))
-        .collect::<Vec<&String>>()
-}
-
-fn get_vertical_word<'a>(
-    word_range: &Range<usize>,
-    letters: &'a Vec<Vec<String>>,
-    row_index: usize,
-    col_index: usize,
-    row_mod: isize
-) -> Vec<&'a String> {
-    return word_range
-        .clone()
-        .filter_map(|i| Some(&letters[(row_index as isize + (row_mod * i as isize)) as usize][col_index]))
-        .collect::<Vec<&String>>();
-}
-
-fn get_diagonal_word<'a>(
+fn get_word<'a>(
     word_range: &Range<usize>,
     letters: &'a Vec<Vec<String>>,
     row_index: usize,
@@ -76,6 +51,7 @@ fn main() {
             if *letter != word[0] {
                 continue;
             }
+
             let space_right = col_index <= row.len() - word.len();
             let space_left = col_index >= word.len() - 1;
             let space_down = row_index <= letters.len() - word.len();
@@ -87,43 +63,21 @@ fn main() {
                 }
             };
 
-            let mut handle_horizontal = |col_mod: isize| {
-                is_target_word(get_horizontal_word(&word_range, row, col_index, col_mod));
-            };
+            let modifiers = [
+                (space_right, 0, 1),
+                (space_left, 0, -1),
+                (space_down, 1, 0),
+                (space_up, -1, 0),
+                (space_right && space_down, 1, 1),
+                (space_right && space_up, -1, 1),
+                (space_left && space_down, 1, -1),
+                (space_left && space_up, -1, -1)
+            ];
 
-            if space_right {
-                handle_horizontal(1);
-            }
-            if space_left {
-                handle_horizontal(-1);
-            }
-
-            let mut handle_vertical = |row_mod: isize| {
-                is_target_word(get_vertical_word(&word_range, &letters, row_index, col_index, row_mod));
-            };
-
-            if space_down {
-                handle_vertical(1);
-            }
-            if space_up {
-                handle_vertical(-1);
-            }
-
-            let mut handle_diagonal = |row_mod: isize, col_mod: isize| {
-                is_target_word(get_diagonal_word(&word_range, &letters, row_index, col_index, row_mod, col_mod));
-            };
-
-            if space_right && space_down {
-                handle_diagonal(1, 1);
-            }
-            if space_right && space_up {
-                handle_diagonal(-1, 1);
-            }
-            if space_left && space_down {
-                handle_diagonal(1, -1);
-            }
-            if space_left && space_up {
-                handle_diagonal(-1, -1);
+            for (has_space, row_mod, col_mod) in modifiers {
+                if has_space {
+                    is_target_word(get_word(&word_range, &letters, row_index, col_index, row_mod, col_mod));
+                }
             }
         }
     }
@@ -146,8 +100,8 @@ fn main() {
             let space_up = row_index > 0;
             
             if space_up && space_down && space_left && space_right {
-                let left_bottom_start = get_diagonal_word(&word_range, &letters, row_index + 1, col_index - 1, -1, 1);
-                let left_top_start = get_diagonal_word(&word_range, &letters, row_index - 1, col_index - 1, 1, 1);
+                let left_bottom_start = get_word(&word_range, &letters, row_index + 1, col_index - 1, -1, 1);
+                let left_top_start = get_word(&word_range, &letters, row_index - 1, col_index - 1, 1, 1);
 
                 let matches_one_direction = |mut found_word: Vec<&String>| {
                     if word == found_word {
